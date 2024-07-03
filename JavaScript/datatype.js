1bd8cc8e1be92b9647834b038ba94a9e26da4a72
sachin kumar <sn08064021@gmail.com>
2024-07-04 00:33:06 +0530

    some datatype

---
+++ JavaScript/datatype.js
@@ -0,0 +1,6 @@
+console.log('Hello World');
+console.log(typeof "inosuke"); // string
+console.log(typeof age); // number
+console.log(typeof isloggedin); // boolean
+console.log(typeof state); // undefined
+console.log(typeof null); // object
