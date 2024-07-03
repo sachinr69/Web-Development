a89762df5d7881ee6a8dcbc4b91f40f8e815ca9f
sachin kumar <sn08064021@gmail.com>
2024-07-04 01:06:35 +0530

    use of variable in js

---
+++ JavaScript/variables.js
@@ -0,0 +1,13 @@
+let firstname = 'sachin'
+let lastname = 'nayak'
+let age = 20
+let isHappy = true
+
+const hoursInDay = 24
+
+let userIntro = 
+    'Hi my name ' +
+     firstname + ' ' 
+     + lastname + 
+     ' I am ' + age +
+      'years old.'
