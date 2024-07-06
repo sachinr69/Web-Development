ee29806708d10ab42eadd445a982b1949d25e15d
sachin kumar <sn08064021@gmail.com>
2024-07-06 22:35:51 +0530

    All math Operators

---
+++ JavaScript/math.js
@@ -0,0 +1,37 @@
+Math.PI
+3.141592653589793
+Math.SQRT2
+1.4142135623730951
+Math.sqrt(16)
+4
+Math.pow(2,3)
+8
+Math.floor(45.5665463)
+45
+Math.floor(-34.345)
+-35
+Math.ceil(2.34)
+3
+Math.round(2.45)
+2
+Math.random(1,25)
+0.40741276685029404
+Math.random()*20
+10.341727174400294
+Math.floor(Math.random()*10)
+8
+ Math.floor(Math.random() * (20 - 10 + 1) + 10)
+16
+
+const width = prompt('Please Enter the width value:')
+const height = prompt('Please Enter the heigth value:')
+console.log(width*height);
+document.write(width*height)
+
+1-Assigment Operator = 
+2-subtract -
+3-addition +
+4-multiplication *
+5-division /
+6-Reminder % 
+7-Exponential !
