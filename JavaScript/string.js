b74fccc1b7aa41d4d09535ad905f4aa2e826a8d5
sachin kumar <sn08064021@gmail.com>
2024-07-06 21:47:29 +0530

    use of string js

---
+++ JavaScript/string.js
@@ -0,0 +1,29 @@
+const message = 'Hello'
+const exrtamessage = '    Hi, I am Sachin     '
+const finalmessage = exrtamessage.trim()
+const uppermessage= message.toUpperCase()
+
+const finalmessageLowercase = exrtamessage.trimStart().toLowerCase()
+const lastFourDigit = '7898'
+//lastFourDigit.padStart(16,'*')
+//'************7898'
+const templateString = `Last four digit of my account number is`
+//templateString.concat(' ',lastFourDigit)
+'************7898'
+===>Methods(Without Argument)
+1-toUpperCase()
+2-toLowerCase()
+3-trim()
+4-trimStart()
+5-trimEnd()
+===>Methods(With Argument)
+1-includes()
+2-indexOf()
+3-replace()
+4-replaceAll()
+5-concat()
+6-padStart()
+7-padEnd()
+8-charAt()
+9-charCodeAt()
+10-split()
