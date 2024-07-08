04882662f3339b8b07be058f86e99a3e6ba3a791
sachin kumar <sn08064021@gmail.com>
2024-07-08 22:45:05 +0530

    Basic use of if Condition

---
+++ JavaScript/ifCondition.js
@@ -0,0 +1,10 @@
+const username = 'Sachin'
+const userAge = 22
+
+console.log(`Name: ${username}` )
+console.log(`Age: ${userAge}`)
+
+if(userAge >= 18 && userAge <=24){
+    console.log('User is a working professional')
+    console.log('He is learning computer science')
+    }
