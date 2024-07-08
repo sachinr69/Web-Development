0fb650866e54365d8c99066874bde5924ae83b27
sachin kumar <sn08064021@gmail.com>
2024-07-08 21:47:09 +0530

    basic comparison

---
+++ JavaScript/comparison.js
@@ -0,0 +1,10 @@
+const userAge1 = '18'
+const userAge2 = 24
+"=="(equal to)
+"==="(equal value and equal type)
+"!="(not equal)
+"!=="(not equal value and not equal type)
+">"(greater than)
+"<"(less than)
+">="(greater or equal to)
+"<="(lesser or equal to)
