cc3eb16a9c9db6bb44931a910d7899b569a0b24b
sachin kumar <sn08064021@gmail.com>
2024-07-18 23:48:18 +0530

    ternary operator

---
+++ JavaScript/ternary.js
@@ -0,0 +1,12 @@
+debugger
+
+ const username = 5 > 2 ? 'Anurag' : 'Sachin'
+
+const gender = 'F'
+
+const userMessage = `${gender === 'F' ? 'She' : 'He'} is college Student.`
+console.log('userMessage');
+
+const result = 10 ? 'Sachin' : 'Nayak' ? '12' : 0
+
+console.log(result);
