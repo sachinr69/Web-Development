cd1158079a7a9c61a8d1276a1c0f001d6ef199f9
sachin kumar <sn08064021@gmail.com>
2024-07-20 14:10:24 +0530

    how to use objects in objects

---
+++ JavaScript/Objects.js
@@ -0,0 +1,34 @@
+const myName = 'Sachin'
+
+const userName1=''
+const usernName2=''
+
+
+const user1 = {
+    firstName: 'Sachin'
+}
+// const user2 = {
+//     firstName: 'Ojas',
+//     lastName: 'Agarwal',
+//     Sachin: 'Developer'
+// }
+
+// console.log(user2.lastName);
+// console.log(user2["firstName"]);
+// console.log(user2[myName]);
+
+const user2 = {
+    firstName: 'Ojas',
+    lastName: 'Agarwal',
+    address:{
+        city: 'Banglore',
+        pincode: '48454',
+        state: 'Karnataka',
+        moredetails:{
+            population: '100mill',
+            area: '4000 sqkm',
+            
+        }
+    }
+}
+
