8670aba3bf29c2dc5709814ecd3711c79d801173
sachin kumar <sn08064021@gmail.com>
2024-07-22 20:42:56 +0530

    Learning of while loop

---
+++ JavaScript/whileLoop.js
@@ -0,0 +1,24 @@
+console.log('Program Started');
+
+
+// let i =1
+
+// while(i<=100){
+//     console.log(i);
+//     i++
+// }
+
+const arr = ['Sachin', 'rahul', 'vansh', 'adarsh', 'ojas']
+
+let i = 0
+
+while(i < arr.length){
+    console.log(`${i+1}. ${arr[i]}`)
+    arr[i] = arr[i] + 'kumar'
+    i++
+}
+// 1. Sachin
+// 2. rahul
+// 3. vansh
+// 4. adarsh
+// 5. ojas
