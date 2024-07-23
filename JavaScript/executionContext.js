bb9c0cde632e243088043cb1876d4cf5bd35c949
sachin kumar <sn08064021@gmail.com>
2024-07-23 17:00:59 +0530

    use of execution context

---
+++ JavaScript/executionContext.js
@@ -0,0 +1,21 @@
+// const username = 'Sachin'
+// const useerAge = 20
+
+// function yo(){
+//     console.log('Hii');
+// }
+
+sayhi()
+
+var username = 'sachin'
+var userage = 20
+
+function yo() {
+    const a = 14
+    const b = 11
+   addtwonumber(7, 9)
+}
+
+function addtwonumber(x, y){
+    return x+y
+}
