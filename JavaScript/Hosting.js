e8f5ca5ce1ca95c917cb91b72c5b3fa9896e23cf
sachin kumar <sn08064021@gmail.com>
2024-07-24 21:27:36 +0530

    hoisting in js

---
+++ JavaScript/Hosting.js
@@ -0,0 +1,19 @@
+console.log(username);
+
+var username = 'Sachin'
+
+hi()
+
+
+//Function Declaration
+function hi(){
+    console.log('Hello');
+}
+
+sayhi()
+
+
+//Function Expression
+var sayhi = function(){
+    console.log('Hii');
+}
