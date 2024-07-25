3fde4174c646a92f2348259dc301d2ac9551187f
sachin kumar <sn08064021@gmail.com>
2024-07-25 22:17:44 +0530

    use of settimeout and setinterval

---
+++ JavaScript/setTimeout&setInterval.js
@@ -0,0 +1,24 @@
+//const hii = 'hello'
+
+setTimeout(`console.log("Hii") 
+           console.log("I am Sachin")`, 500)
+//settimeout convert a string into a code number.
+//if this code run in direct on console than first it will give his set time return value .eg-->5
+
+// const timer1 = setTimeout(`console.log("hi-1")`,1000)
+// const timer2 = setTimeout(`console.log("hi-2")`,0)
+// const timer3 = setTimeout(a,3000)'
+
+
+// const timer1 = setInterval(`console.log("hi-1")`,1000)
+// const timer2 = setInterval(`console.log("hi-2")`,0)
+ const timer3 = setInterval(a,3000)
+
+// clearInterval(timer3)
+
+function a(){
+    console.log('Hello');
+}
+
+// console.log('fsdfgdsg');//it will run first .
+
